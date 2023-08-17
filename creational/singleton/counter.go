baa104c711c3bf57af0b839f986e50f5b52df488
q10357 <isabel@chaparian.no>
2023-08-17 05:01:03 +0200

    added singleton

---
+++ creational/singleton/counter.go
@@ -0,0 +1,49 @@
+package singleton
+
+//https://medium.com/@ramseyjiang_22278/singleton-design-pattern-in-golang-with-unit-tests-324952bdc77f
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+Once is an object that will perform exactly one action.
+A Once must not be copied after first use.
+In the terminology of the Go memory model, the return from f “synchronizes before” the return from any call of once.Do(f).
+https://pkg.go.dev/sync#Once
+*/
+var once sync.Once
+
+type Singleton interface {
+	IncrementByOne() int
+}
+
+type singleton struct {
+	count int
+}
+
+var instance *singleton
+
+func (s *singleton) IncrementByOne() int {
+	s.count++
+	return s.count
+}
+
+// If instance exists, no replicas
+func GetInstance() *singleton {
+	if instance == nil {
+		//init out singleton
+		once.Do(
+			func() {
+				fmt.Println("Initializing singleton instance...")
+				instance = new(singleton)
+			},
+		)
+	} else {
+		//already initialized
+		fmt.Println("Singleton instance already initialized.")
+	}
+
+	return nil
+}
---
+++ creational/singleton/go.mod
@@ -0,0 +1,3 @@
+module github.com/q10357/design-patterns-go/creational/singleton
+
+go 1.20
